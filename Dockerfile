thedanchez/template-solidjs-elysia

chore: template setup
configured with:
 - solid-js
 - elysia
 - bun
 - nvm
 - typescript
 - vitest
 - eslint
 - prettier
 - docker
 - github actions

Dockerfile
@@ -1,0 +1,24 @@
+FROM oven/bun:1.1.29-alpine
+
+WORKDIR /app
+
+COPY package.json bun.lockb ./
+RUN bun install --frozen-lockfile
+
+ARG VERSION
+ENV VITE_VERSION=$VERSION
+
+COPY public ./public
+RUN echo "{ \"version\": \"${VERSION}\" }" > ./public/version.json
+
+COPY index.html ./
+COPY tsconfig.json ./
+COPY vite.config.ts ./
+COPY src ./src
+COPY backend ./backend
+
+RUN bun run build
+
+EXPOSE 8000
+
+CMD ["bun", "start:server"]

thedanchez/template-bun-solidjs-elysia

Dockerfile
@@ -1,0 +1,24 @@
+FROM oven/bun:1.1.42-alpine
+
+WORKDIR /app
+
+COPY package.json bun.lock ./
+RUN bun install --frozen-lockfile
+
+ARG VERSION
+ENV VITE_VERSION=$VERSION
+
+COPY public ./public
+RUN echo "{ \"version\": \"${VERSION}\" }" > ./public/version.json
+
+COPY index.html ./
+COPY tsconfig.json ./
+COPY vite.config.ts ./
+COPY src ./src
+COPY backend ./backend
+
+RUN bun run build
+
+EXPOSE 8000
+
+CMD ["bun", "start:server"]